45cb1aff4c09da416e6ee5ea6feb0e72a22ce1bd
Caique  Augusto <99914098+Caiquekola@users.noreply.github.com>
2023-10-06 16:14:06 -0300

    Add files via upload

--- Programacao Orientada a Objetos/EXCListaProva/EXC6Country.java
+++ Programacao Orientada a Objetos/EXCListaProva/EXC6Country.java
@@ -6,7 +6,7 @@ class Country{
     private int populacao;
     private String nome;
     private double area;
-    private ArrayList<Country> fronteiras = new ArrayList<Country>();
+    ArrayList<Country> fronteiras = new ArrayList<Country>();
     //A Construtor
     public Country(String ISO, String name, double dimensao){
         this.codigoISO = ISO;
@@ -70,15 +70,19 @@ class Country{
         return densidade;
     }
     //F 
-    //Um método que receba um país como parâmetro e retorne a lista 
-    //de vizinhos comuns aos dois países.
+//   Um método que receba um país como parâmetro e retorne a lista 
+//   de vizinhos comuns aos dois países.
     public String vizinhos(Country pais){
-        ArrayList<String> comum = new ArrayList<String>();
-        for (Country i: fronteiras) {
-            if(fronteiras.contains(pais.fronteiras)){
-                comum.add(pais.fronteiras.nome);
+        ArrayList<String> nomes = new ArrayList<String>();
+        for (int i = 0; i < fronteiras.size(); i++) {
+            for (int j = 0; j < pais.fronteiras.size(); j++) {
+                if(this.fronteiras.get(i).getNome().equals(pais.fronteiras.get(j).getNome())){
+                    nomes.add(pais.fronteiras.get(j).getNome());
+                }
             }
         }
+        String nomesLinha = nomes.toString();
+        return nomesLinha;
     }
 }
 public class EXC6Country {
@@ -86,14 +90,26 @@ public class EXC6Country {
     
     public static void main(String[] args) {
         Country brasil = new Country("3166-1","Brasil",8515767.049);
-        Country anonimo = new Country("3166-1","Brasil",8515767.049);
-        Country anonimo2 = new Country("3161-1","Brasil",8515767.049);
+        Country anonimo = new Country("3166-1","China",8515767.049);
+        Country anonimo2 = new Country("3161-1","Italia",8515767.049);
+        Country anonimo3 = new Country("3161-1","Russia",8515767.049);
         System.out.println(brasil.equals(anonimo));
         System.out.println(brasil.equals(anonimo2));
         brasil.adcFronteira(anonimo);
+        brasil.adcFronteira(anonimo3);
+        brasil.adcFronteira(anonimo2);
+        anonimo.adcFronteira(anonimo2);
+        anonimo.adcFronteira(anonimo3);
         System.out.println(brasil.limitofe(anonimo));
         brasil.setPopulacao(200000000);
-        System.out.printf("Densidade: %.2f hab/km2",brasil.densidadePop());
+        System.out.printf("Densidade: %.2f hab/km2\n",brasil.densidadePop());
+        System.out.println(brasil.vizinhos(anonimo));
+        System.out.println(brasil.fronteiras.get(0).getNome());
+        System.out.println(brasil.fronteiras.get(1).getNome());
+        System.out.println(brasil.fronteiras.get(2).getNome());
+        
+        
+     
         
     }
 
