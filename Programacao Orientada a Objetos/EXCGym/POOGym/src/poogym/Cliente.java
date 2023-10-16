09d9cb93bea1804438e856398579113807c3c1e0
caiquekola <caiquekola@hotmail.com>
2023-10-16 15:10:50 -0300

    GymPoo

---
+++ Programacao Orientada a Objetos/EXCGym/POOGym/src/poogym/Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poogym;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Cliente {
+    String nome;
+    String cpf;
+    double altura, peso;
+    
+}
