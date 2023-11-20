a5dbb59cb04575b200a3a76e617b438fa9b67987
Caique  Augusto <99914098+Caiquekola@users.noreply.github.com>
2023-11-20 20:35:02 -0300

    Add files via upload

---
+++ Programacao Orientada a Objetos/Polimorfismo/EXC1/Animal.java
@@ -0,0 +1,16 @@
+package oop.Polimorfismo.EXC1;
+public class Animal {
+        //Atributos, características
+        String nome;
+        int idade;
+        //Comportamentos,metódos
+        public String somAnimal(){
+            String som = "Sem som";
+            return som;
+        }
+        
+        public String acao(){
+            String acao = "Nenhum";
+            return acao;
+        }
+}
