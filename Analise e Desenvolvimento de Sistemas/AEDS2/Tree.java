991d343604d9b7c6a868407b054307e9df9edfe6
Caique  Augusto <99914098+Caiquekola@users.noreply.github.com>
2024-04-02 22:22:53 -0300

    Add files via upload

--- Analise e Desenvolvimento de Sistemas/AEDS2/Tree.java
+++ Analise e Desenvolvimento de Sistemas/AEDS2/Tree.java
@@ -5,32 +5,56 @@ package aeds;
  *
  * @author Caiquekola
  */
-public class Tree<E> {
+public class Tree {
     
     private Node root;
-    
-    public Tree(E key){
+    private int length;
+
+    public Tree(int key){
         createRoot(key);
     }
     public Tree(){
-        root = new Node(null,null,null);
+        createRoot(0);
     }
     
-    public void createRoot(E root){
+    private void createRoot(int root){
         this.root = new Node(root, null,null);
     }
     
-    
-    
-    
+    public int length(){
+        return this.length;
+    }
+    private void addRight(Node root, int item){
+        Node rt = new 
+    }
+    private void addLeft(Node root, int item){
+        
+    }
+    public void add(int item){
+        Node ponter = this.root; 
+        insert(item,ponter);
+    }
+    private boolean insert(int item, Node root){
+        if(length()==0){
+            createRoot(item);
+            return true;
+        }
+        if(item>root.key){
+            if(root.next==null){
+                addRight(root.next,item);
+            }
+                
+        }
+        
+    }
     
 }
 
-class Node<E>{
-    E key;
+class Node{
+    int key;
     Node next;
     Node left;
-    public Node(E key, Node next, Node left){
+    public Node(int key, Node next, Node left){
         this.key = key;
         this.next = next;
         this.left = left;
