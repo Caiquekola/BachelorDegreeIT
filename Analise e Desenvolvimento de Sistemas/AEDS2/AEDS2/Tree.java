53e97e9940d607c2f626f0e606b91f2085dba876
Caique  Augusto <99914098+Caiquekola@users.noreply.github.com>
2024-04-03 18:57:34 -0300

    Update Tree.java

--- Analise e Desenvolvimento de Sistemas/AEDS2/AEDS2/Tree.java
+++ Analise e Desenvolvimento de Sistemas/AEDS2/AEDS2/Tree.java
@@ -1,43 +1,54 @@
-package AEDS2;
 
-public class Tree {
+public class Main{
+    public static void main (String[] args) {
+        Tree arvore = new Tree(5);
+        System.out.println(arvore.size());
+        System.out.println(arvore.add(3));
+        System.out.println(arvore.size());
+        
+    }
+}
+class Tree {
 
     Node root;
-    private int length;
+    private int size;
 
     public Tree(int item) {
         this.root = createRoot(item);
-        this.length = 1;
+        this.size = 1;
     }
 
     public Tree() {
         this.root = createRoot(0);
-        this.length = 0;
+        this.size = 0;
     }
     
-    private int length(){
-        return this.length;
+    public int size(){
+        return this.size;
     }
     private Node createRoot(int item) {
         Node root = new Node(item, null, null);
         return root;
     }
     
-    public void add(int item){
+    public boolean add(int item){
         Node pointer = this.root;
-        insert(item,pointer);
+        return insert(item,pointer);
     }
     //Suponhamos que não há valores duplicados
     //We imagine there are not duplicate values
     private boolean insert(int item, Node root){
-        if(length()==0){
+        if(size()==0){
             this.root = createRoot(item);
+            size++;
             return true;
         } else if(item>root.key){
             
             if(root.right==null){
                 Node aux = new Node(item,null,null);
                 root.right = aux;
+                size++;
+                return true;
             }else{
                 return insert(item,root.right);
             }
@@ -46,12 +57,44 @@ public class Tree {
             if(root.left==null){
                 Node aux = new Node(item,null,null);
                 root.left = aux;
+                size++;
+                return true;
             }else{
                 return insert(item,root.left);
             }
         }
-        return false;
+        
     }
+    
+    public boolean valueExist(int item){
+        Node pointer = this.root;
+        return searchValue(item,pointer);
+    }
+    
+    private boolean searchValue(int item, Node root){
+        
+        if(size()==0){
+            return false;
+        }
+        if(item==root.key){
+            return true;
+        }
+        if(item>root.key){
+            if(root.right==null){
+                return false;
+            }else{
+                return (searchValue(item,root.right));
+            }
+        }else{
+            if(root.left==null){
+                return false;
+            }else{
+                return searchValue(item,root.left);
+            }
+        }
+        
+    }
+    
 }
 
 class Node {
