b7385a7dfdfe3c0d32e173f79458c35f30878064
Caiquekola <caiquekola@hotmail.com>
2024-07-06 15:40:00 -0300

    Application Properties

--- SpringBoot/SpringTheBeggining/todosimple/todosimple/src/main/java/com/caiquekola/todosimple/TodosimpleApplication.java
+++ SpringBoot/SpringTheBeggining/todosimple/todosimple/src/main/java/com/caiquekola/todosimple/TodosimpleApplication.java
@@ -1,4 +1,4 @@
-package com.lucasangelo.todosimple;
+package com.caiquekola.todosimple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
