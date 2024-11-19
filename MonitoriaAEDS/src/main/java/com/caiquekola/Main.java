69aab0c82f0c1c3c4315fbba7993488b9484d8d1
Caiquekola <caiquekola@hotmail.com>
2024-11-19 18:19:37 -0300

    Web Exc 2 e 3

--- MonitoriaAEDS/src/main/java/com/caiquekola/Main.java
+++ MonitoriaAEDS/src/main/java/com/caiquekola/Main.java
@@ -1,7 +1,34 @@
 package com.caiquekola;
 
+import com.caiquekola.ListaEncadeada.ListaDuplamenteEncadeada;
+import com.caiquekola.ListaEncadeada.ListaEncadeada;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Iniciação da lista encadeada
+        ListaEncadeada<Integer> listaEncadeada = new ListaEncadeada<>();
+        //Adição de nós
+        listaEncadeada.addHead(5);
+        listaEncadeada.addHead(6);
+        listaEncadeada.addHead(7);
+        //Impressão
+        listaEncadeada.printLinkedList();
+        //Remoção
+        listaEncadeada.removeElement(6);
+        listaEncadeada.printLinkedList();
+
+        System.out.println("------=-------=-------");
+
+        //Criação da lista duplamente encadeada
+        ListaDuplamenteEncadeada<Integer> listaDuplaEncadeada = new ListaDuplamenteEncadeada<>();
+        listaDuplaEncadeada.addHead(5);
+        listaDuplaEncadeada.addHead(6);
+        listaDuplaEncadeada.addTail(7);
+        //Impressão após adição de elementos
+        listaDuplaEncadeada.printLinkedList();
+
+        listaDuplaEncadeada.removeElement(7);
+        listaDuplaEncadeada.printLinkedList();
     }
 }
